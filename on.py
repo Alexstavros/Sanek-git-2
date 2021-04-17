46f2ac4e47b4913972d66d52ae5e806ab3b119d9
unknown <Alexstavros>
2021-04-17 12:23:46 +0300

    aaa

--- on.py
+++ on.py
@@ -1,8 +1,29 @@
-print(2 + 2)
-print(2 - 2)
-print(2 * 2)
-print(2 / 2)
-print(2 ** 12)
-print(5 // 2)
-print(5 % 3)
+# print(2 + 2)
+# print(2 - 2)
+# print(2 * 2)
+# print(2 / 2)
+# print(2 ** 12)
+# print(5 // 2)
+# print(5 % 3)
 
+# x = 5
+# print (x)
+# x = 'Hellow!'
+# print (x)
+# x = 7
+# type_of_variable = type(x)
+# print(type_of_variable)
+
+# y = 6
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x ** y)
+
+
+credit = 1000
+credit_rate = 10
+number_of_years = 5
+final_sum = credit + credit / 100 * credit_rate * number_of_years
+print(final_sum)
