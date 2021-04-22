9a4897e3fc52cf573244206f333efc3bf9f89b6e
unknown <Alexstavros>
2021-04-22 21:54:33 +0300

    aaa

--- on.py
+++ on.py
@@ -1,17 +1,32 @@
-greeting = "Hello"
-first_name = "Vanysha"
-last_name = "Savchuk"
-exclamation_sign = "!"
-white_space = " "
-print(greeting + white_space + first_name + " " + last_name + exclamation_sign)
-long_string = "This is long string"
-print(long_string)
+#greeting = "Hello"
+#first_name = "Vanysha"
+#last_name = "Savchuk"
+#exclamation_sign = "!"
+#white_space = " "
+#print(greeting + white_space + first_name + " " + last_name + exclamation_sign)
+#long_string = "This is long string"
+#print(long_string)
 
-some_string = "I'm a programmer"
-print(some_string)
-another_string = "I want to learn \"Pynhon\""
-print(another_string)
-string_with_new_lines = "Hellow! \nMy name is YouRa"
-print(string_with_new_lines)
-numbers = "123456789"
-print(numbers)
+#some_string = "I'm a programmer"
+#print(some_string)
+#another_string = "I want to learn \"Pynhon\""
+#print(another_string)
+#string_with_new_lines = "Hellow! \nMy name is YouRa"
+#print(string_with_new_lines)
+#numbers = "12\t 345\t 6789"
+#print(numbers)
+#some_text = "\t Hello! \n\t I'm very glad to see you!"
+#print(some_text)
+
+#string_with_triple_quotes = """This is ' text with "triple quotes" """
+#another_string_with_triple_quotes = '''This is ' text with "triple quotes" '''
+#print(string_with_triple_quotes)
+#print(another_string_with_triple_quotes)
+
+greeting = 'Hello python'
+greeting_lenght = len(greeting)
+print(greeting_lenght)
+print(greeting[1])
+print(greeting[6])
+print(greeting[-1])
+print(greeting[-4])
