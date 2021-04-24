0093df48a34a498ecf956281d2697ee5906a20be
unknown <Alexstavros>
2021-04-24 20:17:06 +0300

    aaa

--- on.py
+++ on.py
@@ -1,32 +1,57 @@
-#greeting = "Hello"
-#first_name = "Vanysha"
-#last_name = "Savchuk"
-#exclamation_sign = "!"
-#white_space = " "
-#print(greeting + white_space + first_name + " " + last_name + exclamation_sign)
-#long_string = "This is long string"
-#print(long_string)
+greeting = "Hello"
+first_name = "Vanysha"
+last_name = "Savchuk"
+exclamation_sign = "!"
+white_space = " "
+print(greeting + white_space + first_name + " " + last_name + exclamation_sign)
+long_string = "This is long string"
+print(long_string)
 
-#some_string = "I'm a programmer"
-#print(some_string)
-#another_string = "I want to learn \"Pynhon\""
-#print(another_string)
-#string_with_new_lines = "Hellow! \nMy name is YouRa"
-#print(string_with_new_lines)
-#numbers = "12\t 345\t 6789"
-#print(numbers)
-#some_text = "\t Hello! \n\t I'm very glad to see you!"
-#print(some_text)
+some_string = "I'm a programmer"
+print(some_string)
+another_string = "I want to learn \"Pynhon\""
+print(another_string)
+string_with_new_lines = "Hellow! \nMy name is YouRa"
+print(string_with_new_lines)
+numbers = "12\t 345\t 6789"
+print(numbers)
+some_text = "\t Hello! \n\t I'm very glad to see you!"
+print(some_text)
 
-#string_with_triple_quotes = """This is ' text with "triple quotes" """
-#another_string_with_triple_quotes = '''This is ' text with "triple quotes" '''
-#print(string_with_triple_quotes)
-#print(another_string_with_triple_quotes)
+string_with_triple_quotes = """This is ' text with "triple quotes" """
+another_string_with_triple_quotes = '''This is ' text with "triple quotes" '''
+print(string_with_triple_quotes)
+print(another_string_with_triple_quotes)
 
 greeting = 'Hello python'
 greeting_lenght = len(greeting)
 print(greeting_lenght)
+print(greeting[0])
 print(greeting[1])
-print(greeting[6])
-print(greeting[-1])
-print(greeting[-4])
+print(greeting[-10])
+print(greeting[-10])
+print(greeting[-8])
+
+print(greeting[2:5])
+print(greeting[6:10])
+print(greeting[7:9])
+print(greeting[-5:-2])
+print(greeting[:7])
+print(greeting[:])
+print(greeting[::2])
+print(greeting[:4])
+print(greeting[::-1])
+
+
+first_name = 'Jake'
+print (first_name[2]) 
+
+first_two_letters = first_name[:2]
+print(first_two_letters)
+last_letter = first_name[-1:]
+print(last_letter)
+
+new_first_name = first_two_letters + 'n' + last_letter
+print(new_first_name)
+
+greeting = 'Hello'
