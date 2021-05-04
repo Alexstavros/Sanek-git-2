ccf236c2389da977f0a0f8a2444d00472bc4af1d
unknown <Alexstavros>
2021-05-04 22:35:52 +0300

    aaa

--- on.py
+++ on.py
@@ -1,61 +1,34 @@
-#first_name = 'Jake'
-#print (first_name[2]) 
-
-#first_two_letters = first_name[:2]
-#print(first_two_letters)
-#last_letter = first_name[-1:]
-#print(last_letter)
-
-#new_first_name = first_two_letters + 'n' + last_letter
-#print(new_first_name)
-
-#greeting = 'Hello'
-#greeting =  greeting + 'Python!'
-#print(greeting)
-
-#yummy = 'Yum'
-#print(yummy * 3)
-
-#print(yummy.upper())
-#print(yummy.lower())
-#print(yummy)
-
-#long_string = 'This is long string'
-#print(long_string.split('s'))
-#print(1 + 1)
-#print('1' + '1')
-#age = 23
-#print('Jack is' + str(age) + 'years old.')
-#print('Jack is' + str(23) + 'years old.')
-######################################################################################################################################
-
 #name = 'Alex'
 #age = 12
 #name_and_age = 'My name is {0}. I\'m  {1} years old.'. format(name, age)
 #print(name_and_age)
-#name_and_age = 'My name is {0}. I\'m  {1} years old.'. format('Alex', 12)
+#name_and_age = F'My name is {name}. I\'m  {age} years old.'
 #print(name_and_age)
-#name_and_age = 'My name is {0}. I\'m  {1} years old.'. format(12,'Alex')
-#print(name_and_age)
-#week_days = 'There are seven day in a week : {}, {}, {}, {}, {}, {}, {}.'\
-#.format('Monday', 'Tyuesday','Wednesday','Thursday','Friday','Saturday','Sanday')
-#print(week_days)
-#week_days = 'There are seven day in a week : {sn}, {sa}, {fr}, {th}, {we}, {ty}, {mo}.'\
-#.format(mo = 'Monday',ty = 'Tyuesday',we = 'Wednesday',th = 'Thursday',fr = 'Friday',sa = 'Saturday',sn = 'Sanday')
-#print(week_days)
-#week_days = 'There are seven day in a week : {sn}, {sn}, {sn}, {sn}, {sn}, {sn}, {sn}.'\
-#.format(mo = 'Monday',ty = 'Tyuesday',we = 'Wednesday',th = 'Thursday',fr = 'Friday',sa = 'Saturday',sn = 'Sanday')
-#print(week_days)
-#######################################################################################################################################
 
-#float_result = 1000 / 7
-#print(float_result)
-#print('The result of division is {0:10.3f}'.format(float_result))
-#print('''
-#{0:10.2f} {1:10.2f} {2:10.2f}
-#{3:10.2f} {4:10.2f} {5:10.2f}
-#{6:10.2f} {7:10.2f} {8:10.2f}
-#'''.format(1.45778, 345.232352, 34.2344, 1234.12,
-#           1.45778, 345.232352, 34.2344, 1234.12,
-#           456.43234))
-########################################################################################################################################
+#print('My name is %s. %s %d years old' % (name, "I'm" , age))
+#########################################################################################################################################
+
+number_list = [32, 21, 48, 34.56]
+print(number_list)
+some_list = [12, 34.334, 'hello']
+print(some_list)
+print(len(some_list))
+print(some_list[1])
+another_list = some_list[:2]
+print(another_list)
+some_list[2] = 'hi'
+print(some_list)
+new_list = some_list + another_list
+########################################################################################
+#new_list[5] = 'new item'
+new_list.append('new item')
+new_list.insert(0, 'start')
+new_list.insert(2, 13)
+#########################################################################################
+new_list.pop(-1)
+new_list.pop(0)
+new_list.pop(-3)
+
+
+#########################################
+print(new_list)
