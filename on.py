c5c4be35dd1347ae8469651135892f2df122400c
unknown <Alexstavros>
2021-05-06 15:32:41 +0300

    aaa

--- on.py
+++ on.py
@@ -1,11 +1,13 @@
-car_prices = {'opel' : 5000, 'tayota' : 7000, 'bmw' : 10000}
-print(car_prices)
-print(car_prices['tayota'])
-car_prices['mazda'] = 4000
-print(car_prices)
-car_prices['opel'] = 2000
-print(car_prices)
-del car_prices['tayota'] 
-print(car_prices)
-car_prices.clear()
-print(car_prices)
+#car_prices = {'opel' : 5000, 'tayota' : 7000, 'bmw' : 10000}
+#print(car_prices)
+#print(car_prices['tayota'])
+#car_prices['mazda'] = 4000
+#print(car_prices)
+#car_prices['opel'] = 2000
+#print(car_prices)
+#del car_prices['tayota'] 
+#print(car_prices)
+#car_prices.clear()
+#print(car_prices)
+
+папа ето не вьо что я сегодня сделал я прошол lists удалил и щас делаю dictionaries
