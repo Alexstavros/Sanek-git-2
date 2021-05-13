fd8cf5a191e340a60e28306afa0db14d31b6f618
unknown <Alexstavros>
2021-05-13 22:44:56 +0300

    aaa

--- on.py
+++ on.py
@@ -1,25 +1,8 @@
-#tuple_1 = 1, 2, 3
-#tuple_2 = ('one, hello')
-#tuple_3 = (3, 2.3, 'three')
-#
-#new_tuple = (tuple_1[0], 3, tuple_1[-1])
-#print(new_tuple)
-#
-#
-#print(tuple_1[1])
-#print(type(tuple_1))
-#print(tuple_2)
-#print(tuple_3)
+rainbow_colors = {'red', 'orange', 'yellow', 'green', 'blue',  'purple', 'indigo'}
 
-x = y = z = 12
-x, y, z = 12, 13, 14
+print(rainbow_colors)
+print(type(rainbow_colors))
 
-print(x, y, z)
-
-person_tuple = ('John', 'Smith', 1986)
-first_name, last_name, year_of_birth = person_tuple
-
-print(first_name, last_name, year_of_birth)
-
-t1 = (1, 2, 5, 1, 7, 9)
-print(t1.count(5))
+empy_set = set()
+print(empy_set)
+print(type(empy_set))
