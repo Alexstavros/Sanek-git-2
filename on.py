78a012c25b2248b0a7593e57a9a4da9567eccb63
unknown <Alexstavros>
2021-05-16 21:51:03 +0300

    aaa

--- on.py
+++ on.py
@@ -6,3 +6,22 @@ print(type(rainbow_colors))
 empy_set = set()
 print(empy_set)
 print(type(empy_set))
+
+empy_set = set()
+print(empy_set)
+print(type(empy_set))
+
+number_list = [1, 43, 56, 3, 3, 3]
+text_tuple = ('sdada', 'adada', 'sasdssad', 'hi', 'hi', 'hi')
+set_from_list =set(number_list)
+set_from_tuple =set(text_tuple)
+
+set_from_list.add(777)
+set_from_tuple.add('hello')
+set_from_list.add(777)
+set_from_tuple.add('hello')
+
+set_from_list.pop()
+
+print(set_from_list)
+print(set_from_tuple)
