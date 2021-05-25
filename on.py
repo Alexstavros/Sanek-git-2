5fc418b0bb990f1f569ccf49346260fcd0bab123
unknown <Alexstavros>
2021-05-25 22:39:56 +0300

    aaa

--- on.py
+++ on.py
@@ -1,8 +1,29 @@
-x = 3
+# x = 4
 
-if x > 3:
-    print('x > 3')
-elif x < 3:
-    print('x < 3')
-else:
-    print('x == 3')
+# if x > 3:
+#     print('x > 3')
+#     print('I\'m happy')
+# elif x < 3:
+#     print('x < 3')
+# else:
+#     print('x == 3')
+
+# day_time = 'evening'
+
+# if day_time == 'morning':
+#     print('Monster wakes up')
+# elif day_time == 'afternoon':
+#     print('Monster is walking')
+# elif day_time == 'evening':
+#     print('Monster is eating')
+# elif day_time == 'night':
+#     print('Monster is sleeping')
+# else:
+#     print('Monster is doing somthing')
+
+# x = 41
+# if x % 2 == 0:
+#     print('x is even')
+# else:
+#     print('x is odd')
+# print('Some text')
