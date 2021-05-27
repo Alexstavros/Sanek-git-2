9ef9d7ff9ca4b4c10086e15232710e101464b8f0
unknown <Alexstavros>
2021-05-27 20:45:56 +0300

    aaa

--- on.py
+++ on.py
@@ -1,29 +1,12 @@
-# x = 4
+# user_input = input('Input something')
+# if user_input == 'Hello':
+#     print('Hello! Nice to see you!')
 
-# if x > 3:
-#     print('x > 3')
-#     print('I\'m happy')
-# elif x < 3:
-#     print('x < 3')
-# else:
-#     print('x == 3')
+# if [1, 3]:
+#     print('Something')
 
-# day_time = 'evening'
-
-# if day_time == 'morning':
-#     print('Monster wakes up')
-# elif day_time == 'afternoon':
-#     print('Monster is walking')
-# elif day_time == 'evening':
-#     print('Monster is eating')
-# elif day_time == 'night':
-#     print('Monster is sleeping')
-# else:
-#     print('Monster is doing somthing')
-
-# x = 41
-# if x % 2 == 0:
-#     print('x is even')
-# else:
-#     print('x is odd')
-# print('Some text')
+lucky_number = input('Enter a number')
+if lucky_number:
+    print(lucky_number + ' is your lucky number !')
+else:
+    print('You have to enter some number , please try again')
