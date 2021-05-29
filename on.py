a09a4e6aa3f0ff4ee469e241460831e859517977
unknown <Alexstavros>
2021-05-29 23:05:25 +0300

    aaa

--- on.py
+++ on.py
@@ -1,12 +1,3 @@
-# user_input = input('Input something')
-# if user_input == 'Hello':
-#     print('Hello! Nice to see you!')
-
-# if [1, 3]:
-#     print('Something')
-
-lucky_number = input('Enter a number')
-if lucky_number:
-    print(lucky_number + ' is your lucky number !')
-else:
-    print('You have to enter some number , please try again')
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in number_list:
+    print(number)
