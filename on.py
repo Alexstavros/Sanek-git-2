284b8cb0101b8e4590e6ff64235824ea8f74f948
unknown <Alexstavros>
2021-07-24 14:49:34 +0300

    aaa

--- on.py
+++ on.py
@@ -16,9 +16,42 @@ anoter_string_with_triple_quotes = '''this is ' with text "triple quotes '''
 print(string_with_triple_quotes)
 print(anoter_string_with_triple_quotes)
 
-greeting = 'hello python'
+greeting = 'hello python!'
 greeting_length = len(greeting)
-print(greeting_length)
+print(len(greeting))
 
-print(greeting[0])
+print(greeting[1])
 print(greeting[6])
+print(greeting[-1])
+print(greeting[-4])
+
+print(greeting[2:5])
+print(greeting[6:10])
+print(greeting[6:10])
+print(greeting[7:13])
+print(greeting[0:12])
+print(greeting[-5:-2])
+print(greeting[5: ])
+print(greeting[:5])
+print(greeting[:])
+print(greeting[::2])
+print(greeting[::1])
+print(greeting[1::3])
+print(greeting[1:9:3])
+print(greeting[::-1])
+
+first_name = 'Jake'
+print(first_name[2])
+first_two_letters = first_name[:2]
+print(first_two_letters)
+last_letter = first_name[-1:]
+print(last_letter)
+
+
+new_first_name = first_two_letters + 'n' + last_letter
+print(new_first_name)
+
+greeting = 'Hello!'
+greeting = greeting + ' Python!'
+print(greeting)
+
